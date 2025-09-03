d2b8a29ed3b9ce98ec179c5e91c5183875b17b8a
Hakim <ahmadhakim6798@gmail.com>
2025-09-03 08:18:15 +0700

    test

--- next.config.js
+++ next.config.js
@@ -1,22 +1,7 @@
 /** @type {import('next').NextConfig} */
-const isProd = process.env.NODE_ENV === "production";
-const repoName = "order-management-app";
 
 const nextConfig = {
   output: "export",
-  trailingSlash: true,
-  // basePath: isProd ? `/${repoName}` : "",
-  // assetPrefix: isProd ? `/${repoName}/` : "",
-  images: {
-    unoptimized: true,
-  },
-  webpack: (config) => {
-    config.module.rules.push({
-      test: /\.svg$/,
-      use: ["@svgr/webpack"],
-    });
-    return config;
-  },
 };
 
 module.exports = nextConfig;
